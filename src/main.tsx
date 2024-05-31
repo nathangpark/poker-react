66439f71a8b41bbdb5463826c87f973f9807384b
nathangpark <155928838+nathangpark@users.noreply.github.com>
2024-05-31 04:43:09 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
+import "./styles/bootstrap.css";
 import "./styles/index.css"
 
 import "./styles/Card.css"
